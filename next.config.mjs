2832f2f67c1f80d1c896ce3aa519c751fbdf3d49
Téllez Valverde Eduardo <eduardotelv@gmail.com>
2025-09-02 15:00:59 -0600

    ManusCript

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
-    const nextConfig = {
-      output: 'export',
-      basePath: '/PortImpl',
-      images: {
-        unoptimized: true,
-      },
-    };
+const nextConfig = {
+  output: 'export', // Exporta a HTML, CSS, y JS estáticos
+  basePath: '/PortImpl', // Asegúrate de que esto coincida con el nombre de tu repositorio
+  assetPrefix: '/PortImpl/', // Prefijo para todos los assets (CSS, JS, imágenes)
+  images: {
+    unoptimized: true, // Deshabilita la optimización de imágenes de Next.js para exportación estática
+  },
+};
 
-    export default nextConfig;
+export default nextConfig;
